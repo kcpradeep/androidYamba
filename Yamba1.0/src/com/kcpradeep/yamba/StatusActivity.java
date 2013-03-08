a0a547478bd53e05d780b300a41a5bb3144932d2
kcpradeep <a0440118@opayq.com>
2013-03-08 20:59:08 +0530

    update twitter

--- Yamba1.0/src/com/kcpradeep/yamba/StatusActivity.java
+++ Yamba1.0/src/com/kcpradeep/yamba/StatusActivity.java
@@ -1,15 +1,44 @@
 package com.kcpradeep.yamba;
 
-import android.os.Bundle;
+import winterwell.jtwitter.Twitter;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
 
+	EditText editStatus;
+	Button buttonUpdate;
+
+	// Called when the activity is first created
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
+
+		// Get values of text and button from view
+		editStatus = (EditText) findViewById(R.id.editStatus);
+		buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+
+		// Register the listener, call "this" when the button is clicked
+		buttonUpdate.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		String status = editStatus.getText().toString(); 
+		Log.d("StatusActivity","OnClick with status"+status);
+		
+		//update it online
+		Twitter twitter = new Twitter("pradeep999","pradeep");
+		twitter.setAPIRootUrl("http://identi.ca/api/");
+		twitter.setStatus(status);
+
 	}
 
 	@Override
