a4f29b272a4a4155ae23cc5260ed55da198c3e5f
kcpradeep <a0440118@opayq.com>
2013-03-08 23:07:29 +0530

    fix status update with Async task

--- Yamba1.0/src/com/kcpradeep/yamba/StatusActivity.java
+++ Yamba1.0/src/com/kcpradeep/yamba/StatusActivity.java
@@ -1,7 +1,9 @@
 package com.kcpradeep.yamba;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,11 +11,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener {
 
+	private static final String TAG = "StatusActivity";
 	EditText editStatus;
 	Button buttonUpdate;
+	Twitter twitter;
 
 	// Called when the activity is first created
 	@Override
@@ -27,18 +32,44 @@ public class StatusActivity extends Activity implements OnClickListener {
 
 		// Register the listener, call "this" when the button is clicked
 		buttonUpdate.setOnClickListener(this);
+
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl(("http://yamba.marakana.com/api"));
 	}
 
 	@Override
 	public void onClick(View v) {
-		String status = editStatus.getText().toString(); 
-		Log.d("StatusActivity","OnClick with status"+status);
-		
-		//update it online
-		Twitter twitter = new Twitter("pradeep999","pradeep");
-		twitter.setAPIRootUrl("http://identi.ca/api/");
-		twitter.setStatus(status);
+		String status = editStatus.getText().toString();
+		Log.d("StatusActivity", "OnClick with status" + status);
 
+		// update it online
+		new PostToTwitter().execute(status);
+		Log.d(TAG, "onClick");
+
+	}
+
+	class PostToTwitter extends AsyncTask<String, Integer, String> {
+		@Override
+		protected String doInBackground(String... statuses) {
+
+			try {
+				winterwell.jtwitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+		}
+
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
+					.show();
+		}
 	}
 
 	@Override
